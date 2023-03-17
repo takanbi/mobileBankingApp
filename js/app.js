0a4bbf9fae092dde137ee38879828644cb6913e6
takanbi <tunji.akanbi@century.edu>
2023-03-17 14:41:10 -0500

    reworked layout in html and finalized css structure, added jQuery

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+let welcomeTop = $("#welcome-top-content");
+let welcomeMiddle = $("#welcome-middle-content");
+welcomeTop.css("visibility", "hidden");
+welcomeMiddle.css("visibility", "hidden");
+  });
