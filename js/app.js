bacb3f29cc426990345ae87bf3b063725a512b0a
takanbi <tunji.akanbi@century.edu>
2023-03-22 10:28:15 -0500

    added login section

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(function(){
 let welcomeTop = $("#welcome-top-content");
 let welcomeMiddle = $("#welcome-middle-content");
+let welcomeBottom = $("#welcome-bottom-content");
 let checking_1 = $("#checking-1");
 let checkingData = $("#checking-data");
 welcomeTop.css("visibility", "hidden");
 welcomeMiddle.css("visibility", "hidden");
-// checking_1.css("visibility", "hidden");
-// checkingData.css("visibility", "hidden");
+welcomeBottom.css("visibility", "hidden");
+checking_1.css("visibility", "hidden");
+checkingData.css("visibility", "hidden");
   });
